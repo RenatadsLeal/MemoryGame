6deac9c2f6dd96339e427fe64c474276a00c10f7
helenaperdigueiro <helena.perdigueiro@gmail.com>
2021-10-06 22:26:49 -0300

    array, duplicate and shuffle cards, avocado \o/
    Co-authored-by: Carolina Hakamada <hakacarol@users.noreply.github.com>
    Co-authored-by: Renata Leal <RenatadsLeal@users.noreply.github.com>

--- js/main.js
+++ js/main.js
@@ -2,64 +2,139 @@ let logo = document.querySelector("#logo");
 let btnStart = document.querySelector("#btnStart");
 let main = document.querySelector("main");
 
-let createDiv = () => document.createElement("div");
-
-let addClassLevelBtn = (div) => div.setAttribute("class", "levelBtn");
-
-let openGame = () => {
-    let gameBoard = createDiv();
-    gameBoard.setAttribute("id", "gameBoard");
-    main.appendChild(gameBoard);
-
-    let levels = createDiv();
-    levels.setAttribute("id", "levels");
-    gameBoard.appendChild(levels);
-
-    let level1 = createDiv();
-    level1.setAttribute("id", "level1");
-    addClassLevelBtn(level1);
-    level1.innerText = "1";
-    levels.appendChild(level1);
-
-    let level2 = createDiv();
-    level2.setAttribute("id", "level2");
-    addClassLevelBtn(level2);
-    level2.innerText = "2";
-    levels.appendChild(level2);
-
-    let level3 = createDiv();
-    level3.setAttribute("id", "level3");
-    addClassLevelBtn(level3);
-    level3.innerText = "3";
-    levels.appendChild(level3);
-
-    let level4 = createDiv();
-    level4.setAttribute("id", "level4");
-    addClassLevelBtn(level4);
-    level4.innerText = "4";
-    levels.appendChild(level4);
-
-    let level5 = createDiv();
-    level5.setAttribute("id", "level5");
-    addClassLevelBtn(level5);
-    level5.innerText = "5";
-    levels.appendChild(level5);
-
-    let level6 = createDiv();
-    level6.setAttribute("id", "level6");
-    addClassLevelBtn(level6);
-    level6.innerText = "6";
-    levels.appendChild(level6);
-
-    let message = createDiv();
-    message.setAttribute("id", "message");
-    message.innerHTML = "Complete levels to unlock new ones";
-    gameBoard.appendChild(message);
-}
-
 btnStart.onclick = function() {
     logo.style.display = "none";
     btnStart.style.display = "none";
 
-    openGame();
+    openBoard();
 }
+
+let createDiv = (id) =>  {
+    let levelDiv = document.createElement("div");
+    levelDiv.setAttribute("id", id);
+    return levelDiv;
+}
+
+let createLevels = (id, number) => {
+    let level = createDiv(id);
+    level.setAttribute("class", "levelBtn");
+    level.innerText = number;
+    return level;
+}
+
+let openBoard = () => {
+    let initialBoard = createDiv("initialBoard");
+    main.appendChild(initialBoard);
+
+    let levels = createDiv("levels");
+    initialBoard.appendChild(levels);
+
+    let level1 = createLevels("level1", 1);
+    levels.appendChild(level1);
+    level1.onclick = () => openLevel1();
+    
+    let level2 = createLevels("level2", 2);
+    levels.appendChild(level2);
+
+    let level3 = createLevels("level3", 3);
+    levels.appendChild(level3);
+    
+    let level4 = createLevels("level4", 4);
+    levels.appendChild(level4);
+
+    let level5 = createLevels("level5", 5);
+    levels.appendChild(level5);
+    
+    let level6 = createLevels("level6", 6);
+    levels.appendChild(level6);
+
+    let message = createDiv("message");
+    message.innerHTML = "Complete levels to unlock new ones";
+    initialBoard.appendChild(message);
+}
+
+const cards = ["https://cdn-icons-png.flaticon.com/512/826/826985.png",
+"../imgs/avocado.png",
+"https://cdn-icons-png.flaticon.com/512/5520/5520474.png",
+"https://cdn-icons-png.flaticon.com/512/5520/5520577.png",
+"https://cdn-icons-png.flaticon.com/512/5520/5520683.png",
+"https://cdn-icons-png.flaticon.com/512/5520/5520649.png"];
+
+let duplicateCards = number => {
+    let selectedCards = cards.slice(0, number);
+    let doubledCards = selectedCards.concat(selectedCards);
+    return doubledCards;
+}
+
+let shuffle = array => {
+    let currentIndex = array.length, randomIndex;
+
+    while(currentIndex != 0) {
+        randomIndex = Math.floor(Math.random() * currentIndex);
+        currentIndex--;
+
+        [array[currentIndex], array[randomIndex]] =  [array[randomIndex], array[currentIndex]];
+    }
+}
+
+let createCard = element => {
+    let index = cards.indexOf(element);
+    let card = document.createElement("div");
+    card.setAttribute("class", `card card${index}`);
+    let image = document.createElement("img");
+    image.setAttribute("src", element);
+    
+    card.appendChild(image);
+    return card;
+}
+
+let cardsCreated = [];
+
+let openLevel1 = () => {
+    initialBoard.style.display = "none";
+
+    let gameBoard = createDiv("boardLevel1");
+    gameBoard.setAttribute("class", "gameBoard");
+    main.appendChild(gameBoard);
+
+    let doubledCards = duplicateCards(6);
+    
+    shuffle(doubledCards);
+
+    for (let element of doubledCards) {
+        let card = createCard(element);
+        gameBoard.appendChild(card);
+        card.onclick = () => {
+            selectedCard = card;
+            console.log(selectedCard);
+        }
+        card.onclick = () => {
+            if(card == selectedCard) {
+                console.log(card);
+                card.remove();
+            }
+        }
+    }
+
+    cardsCreated = document.querySelectorAll(".card");
+    console.log(cardsCreated);
+
+    let selectedCard;
+    // let cardsCreated = document.querySelectorAll(".card");
+
+    cardsCreated.forEach(card => {
+        card.onclick = () => {
+            selectedCard = card;
+            console.log(selectedCard);
+        }
+        card.onclick = () => {
+            if(card == selectedCard) {
+                console.log(card);
+                card.remove();
+            }
+        }
+    })
+}
+
+
+    
